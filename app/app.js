ff71a7cfa5085b3f995e1fdfe9857b1eeb53b1e2
Shyjal Raazi <shyjal99@gmail.com>
2015-02-23 15:00:17 +0530

    first commit

---
+++ app/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('myapp', []);
+app.controller('StoreListing', function() {
+	this.products = phones;
+});
+var phones = [{
+		name: 'iphone',
+		price: 100,
+		image: 'assets/img/iphone.jpg',
+		available:0
+	}, {
+		name: 'mi4',
+		price: 50,
+		image: 'assets/img/mi4.jpg',
+		available:5
+	}, {
+		name: 'oneplus',
+		price: 55,
+		image: 'assets/img/one.jpg',
+		available:9
+	}
+];
